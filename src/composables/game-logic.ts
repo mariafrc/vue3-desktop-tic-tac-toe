1bbbfcc1ddcaf108205e27d2697706f3138bd5f5
Franck Bemaria <franckbemaria@gmail.com>
2022-01-30 10:47:35 +0300

    feat: check for winner

--- src/composables/game-logic.ts
+++ src/composables/game-logic.ts
@@ -2,14 +2,32 @@ import { ref } from "vue";
 
 export type SquareItemValue = "x" | "o" | "";
 
+const WINNING_LINES: number[][] = [
+  // rows
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+
+  // colomns
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+
+  // diagonals
+  [0, 4, 8],
+  [2, 4, 6],
+];
+
 export function useGameLogic() {
   const gameSquare = ref<SquareItemValue[]>([
     "",
     "",
     "",
+
     "",
     "",
     "",
+
     "",
     "",
     "",
@@ -20,6 +38,25 @@ export function useGameLogic() {
   function onSquareClicked(index: number) {
     gameSquare.value[index] = player.value;
     player.value = player.value === "x" ? "o" : "x";
+    const winner = checkForWinner();
+    if (winner) {
+      console.log("game over");
+    }
+  }
+
+  function checkForWinner() {
+    const gameSquareValue = gameSquare.value;
+    for (let winningLine of WINNING_LINES) {
+      const [a, b, c] = winningLine;
+      if (
+        gameSquareValue[a] &&
+        gameSquareValue[a] === gameSquareValue[b] &&
+        gameSquareValue[a] === gameSquareValue[c]
+      ) {
+        return gameSquareValue[a];
+      }
+    }
+    return null;
   }
 
   return {
